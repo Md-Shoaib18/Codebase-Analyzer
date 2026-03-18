8d7ff0857f8bfd71d5144c576f49dc06c65754a7
Md Shoaib <shoaibmohammed206@gmail.com>
2026-03-18 18:33:10 +0530

    Repo clone and scanning service

--- controllers/analyzeController.js
+++ controllers/analyzeController.js
@@ -1,3 +1,6 @@
+import { cloneRepo } from "../services/repoService";
+import { scanFiles } from "../services/scanService";
+
 export const analyzeRepo = async (req, res) => {
   try {
     const { repoUrl } = req.body;
@@ -6,7 +9,15 @@ export const analyzeRepo = async (req, res) => {
       return res.status(400).json({ error: "Repo URL is required" });
     }
 
-    res.json({ message: "Analysis started" });
+    const repoPath = await cloneRepo(repoUrl);
+
+    const files = scanFiles(repoPath);
+
+
+    res.json({ 
+        totalFiles: files.length,
+        files: files.slice(0, 10)
+     });
 
   } catch (error) {
     res.status(500).json({ error: "Server error" });
---
+++ services/scanService.js
@@ -0,0 +1,30 @@
+// services/scanService.js
+
+import fs from "fs";
+import path from "path";
+
+const IGNORED_FOLDERS = ["node_modules", ".git"];
+const VALID_EXTENSIONS = [".js", ".ts", ".jsx", ".tsx"];
+
+export const scanFiles = (dirPath, fileList = []) => {
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach((file) => {
+    const fullPath = path.join(dirPath, file);
+    const stat = fs.statSync(fullPath);
+
+    if (stat.isDirectory()) {
+      if (!IGNORED_FOLDERS.includes(file)) {
+        scanFiles(fullPath, fileList);
+      }
+    } else {
+      const ext = path.extname(fullPath);
+      if (VALID_EXTENSIONS.includes(ext)) {
+        fileList.push(fullPath);
+      }
+    }
+  });
+
+
+  return fileList;
+};
